80575cf52e6042b13243bc79ce46b66d32777926
mayankpathar <mayankpathar49@gmail.com>
2026-07-05 23:01:10 +0530

    Add implementation to copy a linked list and display the copied list

---
+++ lab-9/a1.c
@@ -0,0 +1,97 @@
+// Write a program to copy a linked list.
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int info;
+    struct node *link;
+};
+
+struct node *first1 = NULL;
+struct node *first2 = NULL;
+
+void insertlist()
+{
+    struct node *n1node;
+
+    n1node = (struct node *)malloc(sizeof(struct node));
+
+    printf("Enter the new info: ");
+    scanf("%d", &n1node->info);
+
+    n1node->link = first1;
+    first1 = n1node;
+}
+
+void copylinkedlist()
+{
+    struct node *save, *prev = NULL;
+
+    if (first1 == NULL)
+    {
+        printf("First list is empty.\n");
+        return;
+    }
+
+    save = first1;
+
+    while (save != NULL)
+    {
+        struct node *n2node;
+
+        n2node = (struct node *)malloc(sizeof(struct node));
+        n2node->info = save->info;
+        n2node->link = NULL;
+
+        if (first2 == NULL)
+        {
+            first2 = n2node;
+        }
+        else
+        {
+            prev->link = n2node;
+        }
+
+        prev = n2node;
+        save = save->link;
+    }
+}
+
+void display()
+{
+    struct node *save = first2;
+
+    if (first2 == NULL)
+    {
+        printf("List is empty.\n");
+    }
+    else
+    {
+        printf("\nCopied Linked List:\n");
+        while (save != NULL)
+        {
+            printf("%d ", save->info);
+            save = save->link;
+        }
+    }
+}
+
+int main()
+{
+    int n;
+
+    printf("Enter the number of nodes: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++)
+    {
+        insertlist();
+    }
+
+    copylinkedlist();
+
+    display();
+
+    return 0;
+}
