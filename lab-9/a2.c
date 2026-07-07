576c2054b9efae7bfa4e46d7b5066b69d2787489
mayankpathar <mayankpathar49@gmail.com>
2026-07-07 12:20:18 +0530

    Refactor code structure for improved readability and maintainability

---
+++ lab-9/a2.c
@@ -0,0 +1,83 @@
+// Write a program to reverse a linked list.
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int info;
+    struct node *link;
+};
+
+struct node *first1 = NULL;
+
+
+void firstlinkedlist() {
+    struct node *n1node, *save;
+
+    n1node = (struct node *)malloc(sizeof(struct node));
+
+    printf("Enter the new info: ");
+    scanf("%d", &n1node->info);
+
+    n1node->link = NULL;
+
+    if (first1 == NULL) {
+        first1 = n1node;
+    } else {
+        save = first1;
+        while (save->link != NULL) {
+            save = save->link;
+        }
+        save->link = n1node;
+    }
+}
+void reversellinkedlist()
+{
+   struct node *prev = NULL, *current = first1, *next = NULL;
+
+    while (current != NULL)
+    {
+        next = current->link;
+        current->link = prev; 
+        prev = current;     
+        current = next;       
+    }
+    first1 = prev; 
+}
+void displaylinkedlist()
+{
+    struct node *save;
+
+    if (first1 == NULL)
+    {
+        printf("List is empty.\n");
+        return;
+    }
+
+    save = first1;
+
+    while (save != NULL)
+    {
+        printf("%d\t", save->info);
+        save = save->link;
+    }
+    
+}
+void main(){
+    
+    int n;
+
+    printf("Enter the number of nodes: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++)
+    {
+        firstlinkedlist();
+    }
+
+    reversellinkedlist();
+
+    displaylinkedlist();
+
+    return 0;
+}
