9f56dbd7ac75a3add98a0957ed2c8f044d7c4359
mayankpathar <mayankpathar49@gmail.com>
2026-07-09 22:56:35 +0530

    Add menu-driven program for circular linked list operations

---
+++ lab-11/a1.c
@@ -0,0 +1,5 @@
+// Write a menu driven program to implement following operations on the circular linked list.
+//  Insert a node at the front of the linked list.
+//  Delete a node from specified position.
+//  Insert a node at the end of the linked list.
+//  Display all nodes.
