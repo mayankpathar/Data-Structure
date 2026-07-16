53f2dbe48e0cf8bb41f3bfd6f9f0200770e1ca43
mayankpathar <mayankpathar49@gmail.com>
2026-07-16 18:35:12 +0530

    Refactor circular linked list implementation for consistency and clarity

--- lab-11/a1.c
+++ lab-11/a1.c
@@ -4,109 +4,15 @@
 //  Insert a node at the end of the linked list.
 //  Display all nodes.
 #include<stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
 
-struct Node{
+struct node
+{
     int info;
-    struct Node *link;
+    struct node *link;
 };
-struct Node *first=NULL;
-void insertfirst(){
-    struct Node *newnode;
-    newnode=(struct Node*)malloc(sizeof(struct Node));
-    printf("Enter the new info: ");
-    scanf("%d",&newnode->info);
-    if(first==NULL){
-        first=newnode;
-        newnode->link=first;
-    }
-    else{
-        struct Node *temp=first;
-        while(temp->link!=first){
-            temp=temp->link;
-        }
-        temp->link=newnode;
-        newnode->link=first;
-        first=newnode;
-    }
+struct node *first=NULL;
+struct node *last=NULL;
 
-}
-void insertlast(){
-    struct Node *newnode;
-    newnode=(struct Node*)malloc(sizeof(struct Node));
-    printf("Enter the new info: ");
-    scanf("%d",&newnode->info);
-    if(first==NULL){
-        first=newnode;
-        newnode->link=first;
-    }
-    else{
-        struct Node *temp=first;
-        while(temp->link!=first){
-            temp=temp->link;
-        }
-        temp->link=newnode;
-        newnode->link=first;
-    }
-}
-void deletepos(){
-    int pos;
-    printf("Enter the position to delete: ");
-    scanf("%d",&pos);
-    if(first==NULL){
-        printf("List is empty.\n");
-        return;
-    }
-    struct Node *temp=first;
-    if(pos==1){
-        while(temp->link!=first){
-            temp=temp->link;
-        }
-        struct Node *todelete=first;
-        first=first->link;
-        temp->link=first;
-        free(todelete);
-        return;
-    }
-    for(int i=1;i<pos-1;i++){
-        temp=temp->link;
-        if(temp==first){
-            printf("Position out of bounds.\n");
-            return;
-        }
-    }
-    struct Node *todelete=temp->link;
-    temp->link=todelete->link;
-    free(todelete);
-}
-void display(){
-    if(first==NULL){
-        printf("List is empty.\n");
-        return;
-    }
-    struct Node *temp=first;
-    printf("Circular Linked List: ");
-    do{
-        printf("%d ",temp->info);
-        temp=temp->link;
-    }while(temp!=first);
-    printf("\n");
-}
-void main(){
-    
-    int n;
 
-    printf("Enter the number of nodes: ");
-    scanf("%d", &n);
 
-    for (int i = 1; i <= n; i++)
-    {
-        insertfirst();
-    }
-
-    
-
-    display();
-
-    return 0;
-}
