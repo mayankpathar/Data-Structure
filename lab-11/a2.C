9306ccccc759863d74d536c623c28ae16b12e2eb
mayankpathar <mayankpathar49@gmail.com>
2026-07-20 22:05:35 +0530

    fix code

--- lab-11/a2.C
+++ lab-11/a2.C
@@ -1,88 +1,90 @@
 // WAP to split a circular linked list into two halves.
 #include <stdio.h>
-#include<stdlib.h>
-struct Node{
+#include <stdlib.h>
+
+struct Node {
     int Info;
     struct Node *link;
 };
+
 struct Node *First = NULL;
 struct Node *Last = NULL;
 
-void insert(){
-    struct Node *new;
-    
-    new = (struct Node *)malloc(sizeof(struct Node));
+void insert() {
+    struct Node *node;
+
+    node = (struct Node *)malloc(sizeof(struct Node));
 
     printf("Enter Info: ");
-    scanf("%d", &new->Info);
+    scanf("%d", &node->Info);
 
-    if (First == NULL){
-        First = Last = new;
-        new->link = First;
+    if (First == NULL) {
+        First = Last = node;
+        node->link = First;
     }
-    else
-    {
-        
-        Last->link = new;
-        Last = new;
+    else {
+        Last->link = node;
+        Last = node;
         Last->link = First;
     }
 }
-void splitList(struct Node **First1, struct Node **First2){
+
+void splitList(struct Node **First1, struct Node **First2) {
     struct Node *half, *full;
-    
+
     if (First == NULL)
         return;
 
     half = First;
     full = First;
 
-    while (full->link != First && full->link->link != First)
-    {
+    while (full->link != First && full->link->link != First) {
         half = half->link;
         full = full->link->link;
     }
 
-    if (full->link->link == First){
+    if (full->link->link == First) {
         full = full->link;
     }
+
     *First1 = First;
+    *First2 = NULL;
 
     if (First->link != First)
         *First2 = half->link;
 
     half->link = *First1;
-    full->link = *First2;
+
+    if (*First2 != NULL)
+        full->link = *First2;
+    else
+        full->link = *First1;   
 }
-void display()
-{
+
+void display(struct Node *First) {
     struct Node *temp;
 
-    if (First == NULL)
-    {
+    if (First == NULL) {
         printf("List is empty.\n");
         return;
     }
 
     temp = First;
-    do
-    {
+    do {
         printf("%d -> ", temp->Info);
         temp = temp->link;
-    }
-    while (temp != First);
+    } while (temp != First);
     printf("(Back to First)\n");
-
-    return;
 }
-void main(){
+
+int main() {
     int n;
     struct Node *First1 = NULL;
     struct Node *First2 = NULL;
 
-    printf("ENter number of Nodes: ");
+    printf("Enter number of Nodes: ");
     scanf("%d", &n);
-    for (int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++) {
         insert();
     }
 
@@ -96,5 +98,6 @@ void main(){
 
     printf("\nSecond Half:\n");
     display(First2);
-    return;
+
+    return 0;
 }
