0c74d2cdebe6973731e122e53c4a487a59989409
mayankpathar <mayankpathar49@gmail.com>
2026-07-26 18:37:24 +0530

    prefix code

---
+++ lab-15/a2.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX 100
+
+char stack[MAX];
+int top = -1;
+
+void push(char ch)
+{
+    stack[++top] = ch;
+}
+
+char pop()
+{
+    return stack[top--];
+}
+
+int precedence(char ch)
+{
+    if (ch == '^')
+        return 3;
+    if (ch == '*' || ch == '/')
+        return 2;
+    if (ch == '+' || ch == '-')
+        return 1;
+    return 0;
+}
+
+void reverse(char str[])
+{
+    int i, j;
+    char temp;
+
+    for (i = 0, j = strlen(str) - 1; i < j; i++, j--)
+    {
+        temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+    }
+}
+
+int main()
+{
+    char infix[MAX], prefix[MAX];
+    int i, j = 0;
+
+    printf("Enter Infix Expression: ");
+    scanf("%s", infix);
+
+    reverse(infix);
+
+    for (i = 0; infix[i] != '\0'; i++)
+    {
+        if (infix[i] == '(')
+            infix[i] = ')';
+        else if (infix[i] == ')')
+            infix[i] = '(';
+    }
+
+    for (i = 0; infix[i] != '\0'; i++)
+    {
+        char ch = infix[i];
+
+        if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'))
+        {
+            prefix[j++] = ch;
+        }
+        else if (ch == '(')
+        {
+            push(ch);
+        }
+        else if (ch == ')')
+        {
+            while (stack[top] != '(')
+                prefix[j++] = pop();
+            pop();
+        }
+        else
+        {
+            while (top != -1 && precedence(stack[top]) > precedence(ch))
+                prefix[j++] = pop();
+            push(ch);
+        }
+    }
+
+    while (top != -1)
+        prefix[j++] = pop();
+
+    prefix[j] = '\0';
+
+    reverse(prefix);
+
+    printf("Prefix Expression: %s\n", prefix);
+
+    return 0;
+}
