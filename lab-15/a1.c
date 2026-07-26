62803fffcb1698fcfb2796bca13730ffba16cc07
mayankpathar <mayankpathar49@gmail.com>
2026-07-26 18:27:24 +0530

    new code postfix

---
+++ lab-15/a1.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <ctype.h>
+
+#define MAX 100
+
+char stack[MAX];
+int top = -1;
+
+void push(char ch)
+{
+    stack[++top] = ch;
+}
+
+char pop()
+{
+    return stack[top--];
+}
+
+int precedence(char ch)
+{
+    if (ch == '^')
+        return 3;
+    if (ch == '*' || ch == '/')
+        return 2;
+    if (ch == '+' || ch == '-')
+        return 1;
+    return 0;
+}
+
+int main()
+{
+    char infix[MAX], postfix[MAX];
+    int i = 0, j = 0;
+
+    printf("Enter Infix Expression: ");
+    scanf("%s", infix);
+
+    while (infix[i] != '\0')
+    {
+        if ((infix[i] >= 'A' && infix[i] <= 'Z') ||
+            (infix[i] >= 'a' && infix[i] <= 'z'))
+        {
+            postfix[j++] = infix[i];
+        }
+        else if (infix[i] == '(')
+        {
+            push(infix[i]);
+        }
+        else if (infix[i] == ')')
+        {
+            while (stack[top] != '(')
+            {
+                postfix[j++] = pop();
+            }
+            pop();
+        }
+        else
+        {
+            while (top != -1 && precedence(stack[top]) >= precedence(infix[i]))
+            {
+                postfix[j++] = pop();
+            }
+            push(infix[i]);
+        }
+        i++;
+    }
+
+    while (top != -1)
+    {
+        postfix[j++] = pop();
+    }
+
+    postfix[j] = '\0';
+
+    printf("Postfix Expression: %s\n", postfix);
+
+    return 0;
+}
