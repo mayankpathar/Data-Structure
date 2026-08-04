4bdf02ecad15d84669e98e6d06f4a9f6d26904ea
mayankpathar <mayankpathar49@gmail.com>
2026-08-04 13:22:31 +0530

    Refactor code structure for improved readability and maintainability

--- lab-24/a2.2.c
+++ lab-24/a2.2.c
@@ -1,6 +1,6 @@
 // Write a program to implement a Binary Search using Array. (recursive)
 #include<stdio.h>
-
+    int Binary_Search_recursive(int arr[], int l, int h, int search);
 int main(){
     int n, i, l, h, m, search;
     printf("enter the arr size: ");
@@ -16,21 +16,28 @@ int main(){
     scanf("%d", &search);
     l = 0;
     h = n - 1;
-    while(l <= h){
+    int result = Binary_Search_recursive(arr,l,h,search);
+    printf("Element found at index: %d", result);
+    
+    return 0;
+}
+
+int Binary_Search_recursive(int arr[], int l, int h, int search){
+    int m;
+    if(l <= h){
         m = (l+ h) / 2;
         if(arr[m] == search){
-            printf("Element found at index:", m);
-            break;
+            return m;
         }
         else if(arr[m] < search){
-            l = m+ 1;
+            return Binary_Search_recursive(arr,m+1,h,search);
         }
         else{
-            h = m - 1;
+            return Binary_Search_recursive(arr,l,m-1,search);
         }
     }
-    if(l > h){
+    else{
         printf("Element not found\n");
+        return -1;
     }
-    return 0;
 }
