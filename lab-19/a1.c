9ee7e81ed48e1fe1d232f5a847fda828403985f0
mayankpathar <mayankpathar49@gmail.com>
2026-08-06 13:20:51 +0530

    Add initial implementation of Binary Search Tree (BST) operations menu

---
+++ lab-19/a1.c
@@ -0,0 +1,7 @@
+// Write a menu driven program to implement Binary Search Tree (BST) & perform following operations:
+//  Insert a node
+//  Delete a node
+//  Search a node
+//  Preorder Traversal
+//  Postorder Traversal
+//  Inorder Traversal
